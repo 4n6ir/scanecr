8979edd7936ccaaa60e8f98b791f84a64e8be4f8
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-02-05 15:01:30 +0000

    aws-cdk-lib==2.10.0

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ app = cdk.App()
 
 RemedyScanEcrRepositoriesStack(
     app, 'RemedyScanEcrRepositoriesStack',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = os.getenv('CDK_DEFAULT_REGION')
+    ),
     synthesizer = cdk.DefaultStackSynthesizer(
         qualifier = '4n6ir'
     )
